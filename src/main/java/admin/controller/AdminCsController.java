5d0a01ee470a8a526ae1d1206d434d4f49fdc257
정진규 <rritjy@naver.com>
2023-07-03 15:00:52 +0900

    7/3 진규 푸쉬

--- src/main/java/admin/controller/AdminCsController.java
+++ src/main/java/admin/controller/AdminCsController.java
@@ -124,7 +124,7 @@ public class AdminCsController {
 
 				msg.setSubject("호미짐에서 문의하신 내용의 답변이 등록 되었습니다.");
 				String myIp = request.getLocalAddr();
-				String content = "<a href=\"http://" + myIp +":8080/second_prj/cs\">답변 확인하기</a><br><br>처리 담당자: " + cs.getManager_name();
+				String content = "<a href=\"http://" + myIp +":10062/second_prj/cs\">답변 확인하기</a><br><br>처리 담당자: " + cs.getManager_name();
 				msg.setContent(content, "text/html; charset=UTF-8");
 				Transport.send(msg);
 
